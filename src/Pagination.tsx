74f03dbfdfcce9e122e07e1e11964913d968b997
Kirill Kurentsov <immortalsteel@gmail.com>
2023-05-19 16:04:05 +0300

    refactor: Pagination, Review as class components

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 interface PaginationProps {
   pageNumbers: number[];
@@ -6,16 +6,20 @@ interface PaginationProps {
   currentPage: number;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ pageNumbers, handleClick, currentPage }) => {
-  return (
-    <ul className="pagination">
-      {pageNumbers.map((number) => (
-        <li key={number} id={number.toString()} onClick={handleClick} className={currentPage === number ? 'active' : undefined}>
-          {number}
-        </li>
-      ))}
-    </ul>
-  );
-};
+class Pagination extends Component<PaginationProps> {
+  render() {
+    const { pageNumbers, handleClick, currentPage } = this.props;
+
+    return (
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} id={number.toString()} onClick={handleClick} className={currentPage === number ? 'active' : undefined}>
+            {number}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default Pagination;
--- src/Review.tsx
+++ src/Review.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 interface ReviewProps {
   name: string;
@@ -6,22 +6,28 @@ interface ReviewProps {
   date: string;
 }
 
-const shortenName = (name: string) => {
-  const matches = name.match(/^(\S+)\s+(\S)\S*/);
-  if (matches) {
-    return `${matches[1]} ${matches[2]}.`;
-  }
-  return name;
-};
+interface ReviewState {}
 
-const Review: React.FC<ReviewProps> = ({ name, review, date }) => {
-  return (
-    <li className="review">
-      <div className="review__name">{shortenName(name)}</div>
-      <div className="review__text">{review}</div>
-      <div className="review__date">{date}</div>
-    </li>
-  );
-};
+class Review extends Component<ReviewProps, ReviewState> {
+  shortenName = (name: string) => {
+    const matches = name.match(/^(\S+)\s+(\S)\S*/);
+    if (matches) {
+      return `${matches[1]} ${matches[2]}.`;
+    }
+    return name;
+  };
+
+  render() {
+    const { name, review, date } = this.props;
+
+    return (
+      <li className="review">
+        <div className="review__name">{this.shortenName(name)}</div>
+        <div className="review__text">{review}</div>
+        <div className="review__date">{date}</div>
+      </li>
+    );
+  }
+}
 
 export default Review;
